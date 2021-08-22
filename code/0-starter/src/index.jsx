84b588b25095acd3c70cd455e5e3f8ee68479527
2222-42 <stephen0617@me.com>
2021-08-23 07:19:59 +0900

    feat(index): hello, world!

---
+++ code/0-starter/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'; // enable to use JSX
+import ReactDOM from 'react-dom'; // render application in HTML
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
