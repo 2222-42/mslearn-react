5fd8a9b5ad16908f122f8783968f4ba4a90eb5c4
2222-42 <stephen0617@me.com>
2021-08-24 08:23:33 +0900

    feat(*): use props

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -6,12 +6,26 @@ import RecipeTitle from './RecipeTitle'
 
 function App() {
     // TODO: Add recipe object
+    const recipe = {
+        title: 'Mashed potates',
+        feedback: {
+            rating: 4.8,
+            reviews: 20,
+        },
+        ingredenents: [
+            { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+            { name: '4 Tbsp butter', prepared: false },
+            { name: '1/8 cup heavy cream', prepared: false },
+            { name: 'Salt', prepared: false },
+            { name: 'Pepper', prepared: false },
+        ],
+    }
 
     return (
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
-
+            <RecipeTitle title={recipe.title} />
             {/* TODO: Add IngredientList component */}
 
         </article>
